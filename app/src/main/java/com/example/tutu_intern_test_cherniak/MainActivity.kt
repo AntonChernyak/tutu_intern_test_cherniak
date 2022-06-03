d63082404b28fd964c72d67c55bf2568bd51ca95
AntonChernyak <anton767@yandex.ru>
2022-06-03 11:57:30 +0300

    initial commit. added .gitignore files

---
+++ app/src/main/java/com/example/tutu_intern_test_cherniak/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.tutu_intern_test_cherniak
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
